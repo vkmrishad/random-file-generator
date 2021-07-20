188cbb224b0553290b4aef1100d10f3e59c85134
Mohammed Rishad <mohammedrishad.vk@gmail.com>
2021-07-20 12:15:51 +0530

    frontend scaffold

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,40 @@
+import { Button, Card, Table, Container, Row, Col } from 'react-bootstrap';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="container-main">
+      <Card>
+        <Card.Header as="h5">Random File Generator</Card.Header>
+        <Card.Body>
+          <Button variant="primary">Generate</Button><br /><br />
+          <Card.Text>Link:
+            <Card.Link href="http://google.com" target="_blank">"http://google.com"</Card.Link>
+          </Card.Text>
+          <Button variant="warning">Report</Button><br /><br />
+          <Row xs={2} md={2} lg={2}>
+            <Col>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Type</th>
+                    <th>Count</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 }
 
